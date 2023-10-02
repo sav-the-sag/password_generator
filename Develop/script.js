0d5b28983bddf94afaf07e2aad0e1415ebae1d00
savan <savannahwilliams243@gmail.com>
2023-10-02 16:31:44 -0700

    added assignment code with comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,17 @@
 // Assignment code here
-
+// make a function for generatePassword
+function generatePassword() {
+// add properties using var
+  var length = 8,
+charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+retVal = "";
+// use for loop to execute the code more than once
+for (var i = 0, n = charset.length; i < length; ++i) {
+  retVal += charset.charAt(Math.floor(Math.random() * n));
+}
+// stop the execution of the function by using return
+return retVal;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
