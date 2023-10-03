debf455ba336069e202498705360336d952f5544
savan <savannahwilliams243@gmail.com>
2023-10-02 17:01:16 -0700

    added special characters to charset

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,16 @@
 // Assignment code here
 // make a function for generatePassword
 function generatePassword() {
-// add properties using var
+  // add properties using var
   var length = 8,
-charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-retVal = "";
-// use for loop to execute the code more than once
-for (var i = 0, n = charset.length; i < length; ++i) {
-  retVal += charset.charAt(Math.floor(Math.random() * n));
-}
-// stop the execution of the function by using return
-return retVal;
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
+    retVal = "";
+  // use for loop to execute the code more than once
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  // stop the execution of the function by using return
+  return retVal;
 }
 
 // Get references to the #generate element
