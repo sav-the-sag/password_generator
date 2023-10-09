a8e4b9e251d062bee09cc92b3f2e958ea8edbb2d
savan <savannahwilliams243@gmail.com>
2023-10-09 08:53:23 -0700

    made a prompt that confirms option of sp ch

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,21 @@
 // Assignment code here
-// creating new variable called special characters
+// create new variable for special characters, num, lc, and uc
 var specialCharacters = ["!", "#", "$", "%", "&", "'","(",")","*"];
 var numbers = [0,1,2,3,4,5,6,7,8,9];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K","L", "M","N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K","L", "M","N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // make a function for generatePassword
 function generatePassword() {
 // make a prompt that asks user to enter 8 - 128 characters
-var passwordLength = prompt("Enter 8 to 128 characters")
+var passwordLength = prompt("Enter a number between 8 to 128")
 // make a conditional statement for password length
 if (passwordLength < 8 || passwordLength > 128) {
   alert("Please enter a valid number");
 }
-}
 // make a prompt that confirms option of sp ch, num, lc, or uc
+var option = prompt("Include lowercase, uppercase, numeric, and/or special characters?")
+}
 
 
 // make an empty array that holds info of what user wants
